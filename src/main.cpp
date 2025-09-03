67824f051f0a9d69f7c4d1a491a6fffbb8a81cbe
qingzhixing <qzsqqmail@qq.com>
2025-09-04 03:48:10 +0800

    点灯大师

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
+#include <Adafruit_NeoPixel.h>
+
+static int RGB_PIN = 48;
+static int RGB_NUM = 1;
+
+Adafruit_NeoPixel pixels(RGB_NUM, RGB_PIN, NEO_GRB + NEO_KHZ800);
 
 void setup()
 {
+	pixels.begin();
+	pixels.setBrightness(30);
 }
 
 void loop()
 {
+	pixels.setPixelColor(0, 0, 0, 255);
+	pixels.show();
 }
